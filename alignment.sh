c782a2880416e8eac275ccd9316ae85f560d7d15
Namratha Shivani <124088958+Namratha-shivani@users.noreply.github.com>
2024-10-11 15:27:56 -0500

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -12,10 +12,11 @@ trimmomatic PE -phred33 "provide the name of read 1 fastq file" "provide the nam
 "Name of read2_sample"_paired_trimmed.fastq.gz "Name of read2_sample"_unpaired.fastq.gz \
 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
-
-# Align reads to the human genome (hg38)
+ # Align reads to the human genome (hg38)
 
 bowtie2 -x hg38_index -1 "name of forward reads_trimmed_fastq" -2 "name of reverse reads_trimmed_fastq" -S "name of sample".sam
+
+
 
 
 
